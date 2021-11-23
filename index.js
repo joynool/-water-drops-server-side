c4b4cf4247aaa0fb75b18b1291215d392f411f75
JoynoolAbedinMithoon <mithun55@gmail.com>
2021-11-23 15:37:11 +0600

    comments updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 /*------------------------------------------------
-            Database connection string
+            Database connection state
 --------------------------------------------------*/
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iganj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
